d36b590f8e57fbdd699932cdcc240a4927176a14
flat35hd99 <flat35hd99@gmail.com>
2023-02-09 19:36:16 +0900

    Make vars to local

--- lib/task_queue.sh
+++ lib/task_queue.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-PREFIX_FLOCK_SAMPLE=$HOME/product/flock_sample
-
-lock_file=$PREFIX_FLOCK_SAMPLE/.lock/mutex.lock
-task_queue_file=$PREFIX_FLOCK_SAMPLE/.lock/task_queue.txt
-
 # Append a task to the task queue
 function enqueue_task() {
+    lock_file=$PREFIX_FLOCK_SAMPLE/.lock/task_mutex.lock
+    queue_file=$PREFIX_FLOCK_SAMPLE/.lock/task_queue.txt
+
     task=$1
     (
         # Critical section
         # =========================
         flock -x $lock
-        echo $task >> $task_queue_file
+        echo $task >> $queue_file
         flock -u $lock
         # =========================
     ) {lock}>$lock_file
@@ -20,14 +18,17 @@ function enqueue_task() {
 
 # Print the head of the task queue to stdout
 function dequeue_task() {
+    lock_file=$PREFIX_FLOCK_SAMPLE/.lock/task_mutex.lock
+    queue_file=$PREFIX_FLOCK_SAMPLE/.lock/task_queue.txt
+
     task=$(
     (
         # Critical section
         # =========================
         flock -x $lock
-        first_line=$(head -n 1 $task_queue_file)  
-        tail -n +2 $task_queue_file > $task_queue_file.tmp
-        mv $task_queue_file.tmp $task_queue_file
+        first_line=$(head -n 1 $queue_file)  
+        tail -n +2 $queue_file > $queue_file.tmp
+        mv $queue_file.tmp $queue_file
         flock -u $lock
         # =========================
 
